4f98266406c769ca6ce08dde470b669cc75e6876
TanishaVyas <122625790+TanishaVyas@users.noreply.github.com>
2024-01-09 02:55:18 +0530

    brooooo

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,42 +1,52 @@
-import React from 'react';
-import physioImage from '../components/image-removebg-preview (90).png';
+import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import physioImage from "../components/image-removebg-preview (90).png";
 
 const Cover = () => {
   const containerStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#e6e6fa',
-    
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#e6e6fa",
+    height: "100vh",
+    margin: 0,
+    padding: 0,
   };
   const imageStyle = {
-    maxWidth: '100%',
-    height: 'auto', 
-    marginLeft: '-150px', 
+    maxWidth: "100%",
+    height: "auto",
   };
 
   const physioMizeStyle = {
-    fontSize: '4em', 
-    fontFamily: 'Arial, sans-serif',
-    marginLeft: '100px',
+    fontSize: "4em",
+    fontFamily: "Arial, sans-serif",
+    marginLeft: "100px",
   };
   const textStyle = {
-    fontSize: 'em', 
-    fontFamily: 'Arial, sans-serif',
-    marginLeft: '100px',
-    color: 'grey'
+    fontSize: "em",
+    fontFamily: "Arial, sans-serif",
+    marginLeft: "100px",
+    color: "grey",
   };
 
   return (
-    <div style={containerStyle}>
-      <div className="text-content">
-        <h1 style={physioMizeStyle}>PHYSIO-MIZE</h1>
-        <p style={textStyle}>Optimizing Your Movement, Maximizing Your Life!</p>
+    <Box sx={{ flexGrow: 1 }}>
+      <div style={containerStyle}>
+        <Grid container spacing={2}>
+          <Grid item xs={6} md={8}></Grid>
+          <div className="text-content">
+            <h1 style={physioMizeStyle}>PHYSIO-MIZE</h1>
+            <p style={textStyle}>
+              Optimizing Your Movement, Maximizing Your Life!
+            </p>
+          </div>
+          <div className="image-content" style={imageStyle}>
+            <img src={physioImage} alt="Physiotherapy Image" />
+          </div>
+        </Grid>
       </div>
-      <div className="image-content" style={imageStyle}>
-        <img src={physioImage} alt="Physiotherapy Image" />
-      </div>
-    </div>
+    </Box>
   );
 };
 
