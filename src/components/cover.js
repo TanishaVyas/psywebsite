2d62c2df03d6c32b7202d8985cb653af2cec14f8
TanishaVyas <122625790+TanishaVyas@users.noreply.github.com>
2024-01-09 03:32:36 +0530

    final title in center

--- src/components/cover.js
+++ src/components/cover.js
@@ -16,34 +16,48 @@ const Cover = () => {
   const imageStyle = {
     maxWidth: "100%",
     height: "auto",
+    marginLeft: "auto",
   };
 
   const physioMizeStyle = {
     fontSize: "4em",
     fontFamily: "Arial, sans-serif",
-    marginLeft: "100px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
   };
   const textStyle = {
-    fontSize: "em",
+    fontSize: "1em",
     fontFamily: "Arial, sans-serif",
-    marginLeft: "100px",
     color: "grey",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <div style={containerStyle}>
         <Grid container spacing={2}>
-          <Grid item xs={6} md={8}></Grid>
-          <div className="text-content">
-            <h1 style={physioMizeStyle}>PHYSIO-MIZE</h1>
-            <p style={textStyle}>
-              Optimizing Your Movement, Maximizing Your Life!
-            </p>
-          </div>
-          <div className="image-content" style={imageStyle}>
-            <img src={physioImage} alt="Physiotherapy Image" />
-          </div>
+          <Grid item xs={12} md={6}>
+            {" "}
+            {/* Updated Grid item */}
+            <div className="text-content">
+              <h1 style={physioMizeStyle}>PHYSIO-MIZE</h1>
+              <p style={textStyle}>
+                Optimizing Your Movement, Maximizing Your Life!
+              </p>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            {" "}
+            {/* Updated Grid item */}
+            <div className="image-content" style={imageStyle}>
+              <img src={physioImage} alt="Physiotherapy Image" />
+            </div>
+          </Grid>
         </Grid>
       </div>
     </Box>
