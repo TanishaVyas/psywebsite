b74daf8ec1d49a6254dac3a9a9d613b26807c2bc
TanishaVyas <122625790+TanishaVyas@users.noreply.github.com>
2024-01-10 23:28:13 +0530

    chipi chipi

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,88 +1,97 @@
 import React from "react";
+import { Container, Typography, Paper, Grid } from "@mui/material";
 
-const pageTitleStyle = {
-  textAlign: "left",
-  fontSize: "35px",
-  marginBottom: "20px",
-  marginLeft: "40px",
-};
-
-const contactContainerStyle = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "flex-start",
-  fontSize: "45px",
-  marginBottom: "20px",
-  marginRight: "20px", // Adjusted to add margin on the right
-};
-
-const iconContainerStyle = {
-  color: "black",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  padding: "50px",
-};
-
-const aboutUsContainerStyle = {
+const containerStyle = {
   backgroundColor: "#FCFAF5",
   borderRadius: "15px",
   padding: "50px",
   margin: "50px",
 };
 
+const sectionTitleStyle = {
+  textAlign: "left",
+  fontSize: "35px",
+  marginBottom: "20px",
+};
+
+const contactContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  fontSize: "18px",
+  marginTop: "20px",
+};
+
+const contactInfoStyle = {
+  display: "flex",
+  alignItems: "center",
+  marginBottom: "15px",
+};
+
+const iconStyle = {
+  marginRight: "10px",
+  fontSize: "20px",
+};
+
 function ContactUS() {
   return (
-    <div>
-      <div style={aboutUsContainerStyle}>
-      
-        <div style={pageTitleStyle}>About us:</div>
-        <div style={{ ...pageTitleStyle, fontSize: "15px" }}>
-          <p>
-            Our team is made up of second-year Symbiosis Institute of Technology
-            students pursuing a Bachelor of Computer Science degree. Each member
-            contributes a unique experience covering IoT, data analysis,
-            Artificial Intelligence, web development, 3D modeling, game
-            development, and app development, but they are all united by a shared
-            desire for pioneering improvements in medicine. This diversified skill
-            set serves as the foundation for our collaborative effort to design
-            and manufacture new medical equipment. Driven by a desire to make a
-            tangible difference in healthcare, we work together to develop
-            solutions that push the limits of technical innovation in medicine.
-          </p>
-        </div>
+    <Container style={containerStyle}>
+      <div>
+        <Typography variant="h4" style={sectionTitleStyle}>
+          About us:
+        </Typography>
+        <Typography
+          variant="body1"
+          style={{ ...sectionTitleStyle, fontSize: "15px" }}
+        >
+          Our team is made up of second-year Symbiosis Institute of Technology
+          students pursuing a Bachelor of Computer Science degree. Each member
+          contributes a unique experience covering IoT, data analysis,
+          Artificial Intelligence, web development, 3D modeling, game
+          development, and app development, but they are all united by a shared
+          desire for pioneering improvements in medicine. This diversified skill
+          set serves as the foundation for our collaborative effort to design
+          and manufacture new medical equipment. Driven by a desire to make a
+          tangible difference in healthcare, we work together to develop
+          solutions that push the limits of technical innovation in medicine.
+        </Typography>
       </div>
-      <div style={contactContainerStyle}>
-      <div style={{ display: "flex" ,marginLeft:"100px"}}>
-        <div style={pageTitleStyle}>Contact us:</div></div>
-        <div style={{ display: "flex" ,marginRight:"50px"}}>
-          <div style={iconContainerStyle}>
-            <div style={{ marginLeft: "50px", fontSize: "15px" }}>
-              <b>Email</b>
+
+      <div>
+        <Typography variant="h4" style={sectionTitleStyle}>
+          Contact us:
+        </Typography>
+
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={4}>
+            <div>
+              <Typography variant="subtitle1">
+                <b>Email</b>
+              </Typography>
+              <Typography>yash.parkhi.btech2022@sitpune.edu.in</Typography>
             </div>
-            <span style={{ marginLeft: "5px", fontSize: "15px" }}>
-              yash.parkhi.btech2022@sitpune.edu.in
-            </span>
-          </div>
-          <div style={iconContainerStyle}>
-            <div style={{ marginLeft: "5px", fontSize: "15px" }}>
-              <b>Phone</b>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <div>
+              <Typography variant="subtitle1">
+                <b>Phone</b>
+              </Typography>
+              <Typography>+91 9348243932</Typography>
             </div>
-            <span style={{ marginLeft: "5px", fontSize: "15px" }}>
-              +91 9348243932
-            </span>
-          </div>
-          <div style={iconContainerStyle}>
-            <div style={{ marginLeft: "50px", fontSize: "15px" }}>
-              <b>Linkden</b>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <div>
+              <Typography variant="subtitle1">
+                <b>LinkedIn</b>
+              </Typography>
+              <Typography>
+                https://www.linkedin.com/in/yash-parkhi-737773217/
+              </Typography>
             </div>
-            <span style={{ marginLeft: "50px", fontSize: "15px" }}>
-            https://www.linkedin.com/in/yash-parkhi-737773217/
-            </span>
-          </div>
-        </div>
+          </Grid>
+        </Grid>
       </div>
-    </div>
+    </Container>
   );
 }
 
--- src/components/youtube.js
+++ src/components/youtube.js
@@ -3,20 +3,14 @@ import React from "react";
 const VideoLinks = () => {
   const youtubeVideoLink1 = "https://youtu.be/U-6ukEOxFuw?si=Z0eG_fLrYRlJLE1a";
 
-  const videoContainerStyle = {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr",
-    gap: "16px",
-  };
-
   const wrapperStyle = {
     display: "flex",
     backgroundColor: "#F7F5FD",
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
-    minHeight: "100vh", // Set a minimum height for full-height centering
-    padding: "0 20px", // Add padding to the sides
+    minHeight: "100vh",
+    padding: "0 20px",
   };
 
   const pageTitleStyle = {
@@ -26,30 +20,44 @@ const VideoLinks = () => {
     marginBottom: "20px",
   };
 
-  // Responsive styles using media queries
-  const responsiveVideoContainer = {
-    width: "100%", // Make the video container 100% width
+  const videoContainerStyle = {
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    gap: "16px",
   };
 
-  const responsivePageTitle = {
-    fontSize: "30px", // Adjust font size for smaller screens
+  const iframeStyle = {
+    width: "100%",
+    height: "315px",
+  };
+
+  const videoInfoStyle = {
+    fontSize: "20px",
+    width: "100%", // Adjusted for responsiveness
+    marginTop: "20px",
   };
 
   return (
     <div style={wrapperStyle}>
-      <div style={pageTitleStyle}>DEMO VIDEO</div>
-      <div style={{ ...videoContainerStyle, ...responsiveVideoContainer }}>
-        <iframe
-          width="100%" // Make the iframe 100% width
-          height="315"
-          src={youtubeVideoLink1.replace("youtu.be/", "www.youtube.com/embed/")}
-          title="Embedded YouTube Video 1"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-        <div>
-          <p style={{ fontSize: "20px", width: "600px", marginLeft: "20px" }}>
+      <div style={pageTitleStyle}>
+        <b>DEMO VIDEO</b>
+      </div>
+      <div style={videoContainerStyle}>
+        <div style={{ width: "100%" }}>
+          <iframe
+            style={iframeStyle}
+            src={youtubeVideoLink1.replace(
+              "youtu.be/",
+              "www.youtube.com/embed/"
+            )}
+            title="Embedded YouTube Video 1"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div style={videoInfoStyle}>
+          <p>
             <b>
               This video is about a project called Physio-Mize, which aims to
               bridge the gap between physiotherapists and remote patients.
