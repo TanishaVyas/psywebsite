11fbe840fb8552e59510489de0e873938f28384a
satvikibudhia <122771370+satvikibudhia@users.noreply.github.com>
2024-01-10 23:03:43 +0530

    gippy gippy jaapa jaapa

--- src/components/clip.js
+++ src/components/clip.js
@@ -3,23 +3,38 @@ import vid1 from "../image/clip1.gif";
 import vid2 from "../image/clip2.gif";
 
 function Clip() {
+  const containerStyle = {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around", // Adjust as needed
+    alignItems: "center",
+  };
+
+  const gifContainerStyle = {
+    marginBottom: "20px", // Adjust the padding between GIFs
+    textAlign: "center",
+  };
+
+  const gifStyle = {
+    width: "auto",
+    height: "80vh",
+  };
+
   return (
-    <div style={{ height: "100vh" }}>
-      <img
-        src={vid}
-        alt="GIF Clip"
-        style={{ width: "100px", height: "auto" }}
-      ></img>
-      <img
-        src={vid1}
-        alt="GIF Clip"
-        style={{ width: "100px", height: "auto" }}
-      ></img>
-      <img
-        src={vid2}
-        alt="GIF Clip"
-        style={{ width: "100px", height: "auto" }}
-      ></img>
+    <div style={containerStyle}>
+      <div style={gifContainerStyle}>
+        <img src={vid} alt="GIF Clip" style={gifStyle} />
+        <p>3D visualisation of movement</p>
+      </div>
+      <div style={gifContainerStyle}>
+        <img src={vid1} alt="GIF Clip" style={gifStyle} />
+        <p>360 rotating 3d model of the joint</p>
+      </div>
+      <div style={gifContainerStyle}>
+        <img src={vid2} alt="GIF Clip" style={gifStyle} />
+        <p>Pain button which can mark the intensity of pain</p>
+      </div>
     </div>
   );
 }
